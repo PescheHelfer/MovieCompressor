b1affd7df4d4afd38a987a5398de645fa17340ae
Pesche Helfer <pesche_h@bluewin.ch>
2019-03-09 01:02:48 +0100

    Supress creation of _original , renamed _cmp to _c, variable renaming

--- MovieCompressor.py
+++ MovieCompressor.py
@@ -3,70 +3,80 @@ import re
 import os
 from datetime import datetime, timedelta
 
-movie = r'F:\TestMovies\096_Pana_P1200692.MP4'
-
+movie_path_in = r'F:\TestMovies\096_Pana_P1200692.MP4'
 
 # ToDo: Folder or File as Input Paramter. If Folder is passed: process all movie files, otherwise only single movie
 # ToDo: (Optional) as default: use folder from which script is called
 # ToDo: Implement some kind of progress bar, at least when looping through file list
-# ToDo: Best practice: variable naming in different scopes (ok to have same name globaly as within the function? Use underscores?)
-# ToDo: Änderungsdatum wird irgendwann nach applien der Metadaten doch noch auf heute gesetzt. Andere Reihenfolge wählen? Separt ausführen?
-# ToDo: Erstellen von _original unterdrücken
+# DONE: Erstellen von _original unterdrücken
 
-def get_metadata(movie):
+
+def get_metadata(movie_path):
     """Returns metadata as a dictionary."""
-    p1 = subprocess.Popen(["exiftool", movie],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    metadata_str = p1.communicate()[0].decode("utf-8")  # class 'bytes', sequence of bytes, similar to string, but ASCII only and immutable
-    rx = re.compile('\W+\: ') # regex to remove whitespace after the key (before the colon)
-    metadata_list = [rx.sub(': ',item) for item in metadata_str.split("\r\n")]
-    metadata_dict = dict(item.split(": ") for item in metadata_list[:-1]) # the last item is an ' and can't be split
+    p1 = subprocess.Popen(["exiftool", movie_path],
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # class 'bytes', sequence of bytes, similar to string, but ASCII only and immutable
+    metadata_str = p1.communicate()[0].decode("utf-8")
+    # regex to remove whitespace after the key (before the colon)
+    rx = re.compile('\W+\: ')
+    metadata_list = [rx.sub(': ', item) for item in metadata_str.split("\r\n")]
+    # the last item is an ' and can't be split
+    metadata_dict = dict(item.split(": ") for item in metadata_list[:-1])
     return metadata_dict
 
-def compress_movie(movie):
-    movie_lst = os.path.splitext(movie)
-    movie_cmp = movie_lst[0]+'_cmp'+movie_lst[1]
-    command = 'ffmpeg -i "'+movie+'" -c:v libx264 -crf 23 -c:a aac -map_metadata 0 "'+movie_cmp+'"'
+
+def compress_movie(movie_path):
+    movie_lst = os.path.splitext(movie_path)
+    movie_cmp = movie_lst[0]+'_c'+movie_lst[1]
+    command = 'ffmpeg -i "{0}" -c:v libx264 -crf 23 -c:a aac -map_metadata 0 "{1}"'.format(
+        movie_path, movie_cmp)
    # print(command)
     subprocess.check_call(command)
     return movie_cmp
 
-def set_metadata(movie_cmp, metadata):
+
+def set_metadata(movie_path, metadata):
     """Sets all the dates to the file modification date of the original movie and restores make/model information."""
-    
+
     target_date_time_str = metadata['File Modification Date/Time']
-    target_date_time_adj_str = target_date_time_str[:19] # [:19] -> remove offset
+    # [:19] -> remove offset
+    target_date_time_adj_str = target_date_time_str[:19]
 
     metadata_to_use_dict = {
-		"File Modification Date/Time" : ["-FileModifyDate",target_date_time_str],
-		"File Creation Date/Time" : ["-FileCreateDate",target_date_time_str],
-		"Creation Date" : ["-CreationDate",target_date_time_str],
-		"Media Create Date" : ["-MediaCreateDate",target_date_time_adj_str],
-		"Media Modify Date" : ["-MediaModifyDate",target_date_time_adj_str],
-		"Create Date" : ["-CreateDate",target_date_time_adj_str],
-        "Modify Date" : ["-ModifyDate",target_date_time_adj_str],
-        "Track Create Date" : ["-TrackCreateDate",target_date_time_adj_str],
-        "Track Modify Date" : ["-TrackModifyDate",target_date_time_adj_str],
-        "Make" : ["-Make",""],
-        "iModel" : ["-Model",""],
-        "Software" : ["-Software",""],
-        "Rotation" : ["-Rotation",""]
+        "File Modification Date/Time": ["-FileModifyDate", target_date_time_str],
+        "File Creation Date/Time": ["-FileCreateDate", target_date_time_str],
+        "Creation Date": ["-CreationDate", target_date_time_str],
+        "Media Create Date": ["-MediaCreateDate", target_date_time_adj_str],
+        "Media Modify Date": ["-MediaModifyDate", target_date_time_adj_str],
+        "Create Date": ["-CreateDate", target_date_time_adj_str],
+        "Modify Date": ["-ModifyDate", target_date_time_adj_str],
+        "Track Create Date": ["-TrackCreateDate", target_date_time_adj_str],
+        "Track Modify Date": ["-TrackModifyDate", target_date_time_adj_str],
+        "Make": ["-Make", ""],
+        "iModel": ["-Model", ""],
+        "Software": ["-Software", ""],
+        "Rotation": ["-Rotation", ""]
 
-	}
+    }
     # handler_vendor_id = metadata['Handler Vendor ID'] # not writeable :(
-    stringbuilder = ["exiftool"] # list to join to create the final command string in the end
+    # list to join to create the final command string in the end
+    stringbuilder = ["exiftool"]
 
     for key, value in metadata_to_use_dict.items():
         if(key in metadata):
-            stringbuilder.append(value[0]+'="'+(metadata[key] if value[1]=="" else value[1])+'"')
+            stringbuilder.append(
+                value[0]+'="'+(metadata[key] if value[1] == "" else value[1])+'"')
 
-    stringbuilder.append('"'+movie_cmp+'"')
-    command = " ".join(stringbuilder) # note: the space in " " is actually the seperator!
+    # prevents creation of _original file
+    stringbuilder.append('-overwrite_original')
+    stringbuilder.append('"'+movie_path+'"')
+    # note: the space in " " is actually the seperator!
+    command = " ".join(stringbuilder)
     subprocess.check_call(command)
 
-metadata = get_metadata(movie)
-movie_cmp = compress_movie(movie)
-set_metadata(movie_cmp, metadata)
+
+metadata = get_metadata(movie_path_in)
+movie_path_out = compress_movie(movie_path_in)
+set_metadata(movie_path_out, metadata)
 # print(metadata)
 # print(metadata['file Modification Date/Time'])
-
-
--- testing.py
+++ testing.py
@@ -1,30 +1,35 @@
-import hachoir # not used, exiftool is the way to go
+import hachoir  # not used, exiftool is the way to go
 import subprocess
 import datetime
-import os.path, time
+import os.path
+import time
 import re
 
 file = r'F:\TestMovies\IMG_0185.MOV'
 
-p1 = subprocess.Popen(["exiftool", file],stdout=subprocess.PIPE)
-output = p1.communicate()[0] # class 'bytes', sequence of bytes, similar to string, but ASCII only and immutable
-output_str = output.decode("utf-8") 
+p1 = subprocess.Popen(["exiftool", file], stdout=subprocess.PIPE)
+# class 'bytes', sequence of bytes, similar to string, but ASCII only and immutable
+output = p1.communicate()[0]
+output_str = output.decode("utf-8")
 
-rx = re.compile('\W+\: ') # regex to remove whitespace after the key (before the colon)
+# regex to remove whitespace after the key (before the colon)
+rx = re.compile('\W+\: ')
 
-output_list = [rx.sub(': ',item) for item in output_str.split("\r\n")]
-output_dict = dict(item.split(": ") for item in output_list[:-1]) # the last item is an ' and can't be split
+output_list = [rx.sub(': ', item) for item in output_str.split("\r\n")]
+# the last item is an ' and can't be split
+output_dict = dict(item.split(": ") for item in output_list[:-1])
 
 output_dict['File Creation Date/Time']
 
-## ExifTools Parameter
+# ExifTools Parameter
 # -overwrite_original_in_place (in which case the contents of the output file are used to overwrite the original, and the output file is deleted).
 
 
-def get_media_properties(filename): # hachoir not the way to go, use exiftools intead (see above)
+# hachoir not the way to go, use exiftools intead (see above)
+def get_media_properties(filename):
 
-    result = subprocess.Popen(['hachoir-metadata', filename, '--raw'],# '--level=3'],
-        stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    result = subprocess.Popen(['hachoir-metadata', filename, '--raw'],  # '--level=3'],
+                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
     results = result.stdout.read().decode('utf-8').split('\r\n')
 
@@ -37,10 +42,13 @@ def get_media_properties(filename): # hachoir not the way to go, use exiftools i
         if item.startswith('- duration: '):
             duration = item.lstrip('- duration: ')
             if '.' in duration:
-                t = datetime.datetime.strptime(item.lstrip('- duration: '), '%H:%M:%S.%f')
+                t = datetime.datetime.strptime(
+                    item.lstrip('- duration: '), '%H:%M:%S.%f')
             else:
-                t = datetime.datetime.strptime(item.lstrip('- duration: '), '%H:%M:%S')
-            seconds = (t.microsecond / 1e6) + t.second + (t.minute * 60) + (t.hour * 3600)
+                t = datetime.datetime.strptime(
+                    item.lstrip('- duration: '), '%H:%M:%S')
+            seconds = (t.microsecond / 1e6) + t.second + \
+                (t.minute * 60) + (t.hour * 3600)
             properties['duration'] = round(seconds)
 
         if item.startswith('- width: '):
@@ -51,11 +59,12 @@ def get_media_properties(filename): # hachoir not the way to go, use exiftools i
 
     return properties
 
+
 properties = get_media_properties(file)
 
 print(properties)
 # creation_date und last_moditifaction entsprechen dem (falschen) Ursrungsdatum (Ursprung -> Medium erstellt), nicht den korrigierten Datumsangaben des Files
-# //DONE: Wie kommt man an die Datumsangaben des Files ran? -> see below
+# DONE: Wie kommt man an die Datumsangaben des Files ran? -> see below
 
 
 # get creation and modification date of the file (the one I corrected, not the same as the one from metadata)
