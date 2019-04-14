5f2d837ad6f964e0dc246fa7c6650603bb6f1e55
Pesche Helfer <pesche_h@bluewin.ch>
2019-04-14 23:32:43 +0200

    Modified batch to handle paths with spaces and optional arguments.

--- MovieCompressor.bat
+++ MovieCompressor.bat
@@ -1,10 +1,13 @@
 @ECHO OFF
 CALL activate MovieCompressor
+ECHO !!! Paths with spaces must be enclosed in quotation marks !!!
 
 REM if no parameters are passed, pass current directory to script
 REM %* will return the remainder of the command line starting at the first command line argument
 REM %CD% returns the current directory
-IF "%*"=="" (SET params="%CD%")ELSE (SET params="%*")
+REM IF "%*" == "" does not work if the path contains spaces and has to be quoted. [%*] still does not work.
+REM checking the first param is enough to see if any param has been passed.
+IF [%1] == [] (SET params="%CD%")ELSE (SET params=%*)
 
 REM ECHO 1 rest: %*
 REM Echo 2 CD: %CD%
