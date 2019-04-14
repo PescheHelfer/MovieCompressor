7ccffbb72c52c68a177b87fece9b5438fe08fb6a
Pesche Helfer <pesche_h@bluewin.ch>
2019-04-15 00:03:01 +0200

    Added a color warning to the batch file that the path must not end with a backslash.

--- MovieCompressor.bat
+++ MovieCompressor.bat
@@ -1,6 +1,16 @@
 @ECHO OFF
 CALL activate MovieCompressor
-ECHO !!! Paths with spaces must be enclosed in quotation marks !!!
+REM Enabling colors https://www.dostips.com/forum/viewtopic.php?t=1707
+SETLOCAL DisableDelayedExpansion
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do rem"') do (
+  set "DEL=%%a"
+)
+
+call :ColorText 0e "Paths with spaces must be enclosed in quotation marks"
+call :ColorText 0e "and must NOT end with a backslash!"
+REM blank line:
+ECHO(
+
 
 REM if no parameters are passed, pass current directory to script
 REM %* will return the remainder of the command line starting at the first command line argument
@@ -16,3 +26,12 @@ REM ECHO 3 params: %params%
 python f:\Libraries\Pesche\Docs_Pesche\Coding\Python\MovieCompressor\MovieCompressor.py %params%
 CALL conda deactivate
 REM PAUSE
+goto :eof
+
+:ColorText
+echo off
+<nul set /p .=. > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+echo(%DEL%%DEL%%DEL%
+del "%~2" > nul 2>&1
+goto :eof
